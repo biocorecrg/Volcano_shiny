3de230c6b76e77612aa2ccdf7029d1aefb4212fb
Sarah Bonnin <sarah.bonnin@crg.eu>
2018-08-23 19:08:57 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -1,11 +1,6 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+### Shiny app for building Volcano plots
+# Author: Sarah Bonnin - sarah.bonnin@crg.eu
+
 
 library(shiny)
 library(ggplot2)
@@ -20,19 +15,21 @@ library(shinyWidgets) # for downloadBttn
 #                            stringsAsFactors = F)
 
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws a volcano plot
 ui <- fluidPage(
    
    # Application title
    titlePanel("Volcano plot"),
    
-   # Sidebar with a slider input for the p-value threshold
+   # Sidebar with options
    sidebarLayout(
       sidebarPanel(
+        # Input file can be in text or csv format
         fileInput("file_volcano", "Upload text or csv file", accept = c("text/txt", "text/csv")),
         
-        uiOutput("volcano_columns1"),
-        uiOutput("volcano_columns2"),
+        # Once the user file is loaded, the numeric columns are retrieved and put as options for the user to select the appropriate ones
+        uiOutput("volcano_columns1"), # P-value column
+        uiOutput("volcano_columns2"), # Fold change column
         # P-value threshold for horizontal bar and points color
         sliderInput("volcano_pval_threshold",
                      "P-value threshold:",
@@ -43,13 +40,14 @@ ui <- fluidPage(
                      "fold change threshold:",
                      min = -10, max = 10,
                      value = c(-2,2), step=0.1),
-        # Plot title and title size
+        # Set plot title and title size
         textInput("volcano_title", label = h4("Plot title"), value = "Volcano plot"),
         sliderInput("volcano_size_title",
                     "Title font size:",
                     min = 10, max = 60,
                     value = 2, step=2),
         textInput("volcano_xlab", label = h4("y-axis label"), value = "log2(fold-change)"),
+        # X and y-axis labels and label size
         sliderInput("volcano_size_xlab",
                     "x-axis font size:",
                     min = 6, max = 40,
@@ -59,16 +57,19 @@ ui <- fluidPage(
                     "y-axis font size:",
                     min = 6, max = 40,
                     value = 6, step=2),
-        colourpicker::colourInput("volcano_color_up", "Color for up-regulated genes", "red", allowTransparent = TRUE),
-        colourpicker::colourInput("volcano_color_dw", "Color for down-regulated genes", "green", allowTransparent = TRUE),
-        colourpicker::colourInput("volcano_color_none", "Color for unchanged genes", "black", allowTransparent = TRUE)
+        # Colour chosen for down-, up- and non-regulated genes (selected according to above criteria)
+        colourpicker::colourInput("volcano_color_up", "Color for up-regulated genes", value="red", allowTransparent = TRUE),
+        colourpicker::colourInput("volcano_color_dw", "Color for down-regulated genes", value="green", allowTransparent = TRUE),
+        colourpicker::colourInput("volcano_color_none", "Color for unchanged genes", value="black", allowTransparent = TRUE)
       ),
-      
-      # Show a plot of the generated distribution
+       # Show the plot when the button "Display Volcano plot" is clicked
       mainPanel(
         actionBttn("doVolcano", "Display Vocano plot", icon("bar-chart-o"), style = "jelly", size = "sm"),
          plotOutput("volcanoPlot"),
-         downloadBttn("downloadVolcano", "Save Volcano plot", size = "sm", style = "jelly")
+        conditionalPanel( # if the user clicked on "display Volcano plot", the download button also appears
+          condition = "input.doVolcano == true",
+          downloadBttn("downloadVolcano", "Save Volcano plot", size = "sm", style = "jelly")
+        )
       )
    )
 )
@@ -76,6 +77,7 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  # Reactive function to read in user input file
   input_volcano <- reactive({
     inFile <- input$file_volcano
     if (is.null(inFile))
@@ -85,6 +87,7 @@ server <- function(input, output) {
   })
   shinyjs::enable("doVolcano")
   
+  # Observe if input file was read and output numeric columns choices
   observe({
     if(!is.null(input$file_volcano)){
       volcano1 <- input_volcano()
@@ -97,7 +100,7 @@ server <- function(input, output) {
       output$volcano_columns2 <- renderUI({
         columns_volcano <- colnames(volcano1[unlist(lapply(volcano1, is.numeric))])
         selectInput(inputId="volcano_fc_col", "Fold change column", choices=columns_volcano, selected=input$volcano_fc_col)
-      })  
+      })
     }
   })
   
@@ -107,32 +110,20 @@ server <- function(input, output) {
      if (is.null(input$file_volcano))
        return(NULL)
       
+      # Retrieve user input / data frame
       volcano1 <- input_volcano()
-     
-     # volcano1 <- input_volcano()
-     # 
-     # output$volcano_columns1 <- renderUI({
-     #   columns_volcano <- colnames(volcano1[unlist(lapply(volcano1, is.numeric))])
-     #   selectInput(inputId="volcano_pval_col", "P-value column", choices=columns_volcano, selected=input$volcano_pval_col)
-     # })
-     # 
-     # output$volcano_columns2 <- renderUI({
-     #   columns_volcano <- colnames(volcano1[unlist(lapply(volcano1, is.numeric))])
-     #   selectInput(inputId="volcano_fc_col", "Fold change column", choices=columns_volcano, selected=input$volcano_fc_col)
-     # })  
      
      if(is.null(input$volcano_pval_threshold) | is.null(input$volcano_fc_threshold))
        return(NULL)
-     # rerieve limits for drawing lines and setting colours
+     # rerieve limits for drawing lines and setting colours for up-, down- and non-regulated genes
      pval_threshold  <- input$volcano_pval_threshold
      volcano_fc_threshold_up <- input$volcano_fc_threshold[2]
      volcano_fc_threshold_dw <- input$volcano_fc_threshold[1]
 
       # Convert p-value to -log10 p-value
-     volcano1[input$volcano_pval_col] <- -log10(volcano1[input$volcano_pval_col])
-
+      volcano1[input$volcano_pval_col] <- -log10(volcano1[input$volcano_pval_col])
      
-      ## If the user chooses the option to color points according to down/up-regulated, add a column to the data frame
+      # If the user chooses the option to color points according to down/up-regulated, add a column to the data frame
        volcano1$select <- "Not regulated"
        volcano1$select[!is.na(volcano1[input$volcano_pval_col]) & volcano1[input$volcano_pval_col] >= -log10(pval_threshold) &
                          volcano1[input$volcano_fc_col] >= volcano_fc_threshold_up] <- "Up-regulated"
@@ -153,22 +144,28 @@ server <- function(input, output) {
    })
     
     observeEvent(input$doVolcano, {
+      # If the user clicks on the "Display Vocano" button, draw volcano plot
       output$volcanoPlot <- renderPlot({
         drawVolcano()
       })
+      # The option to export Volcano plot appears at the same time as the plot itself
+      output$downloadVolcano <- downloadHandler(
+        filename = function(){paste(Sys.Date(), "_Volcano.png", sep ="")},
+        content = function(file){
+          png(file) # picking up correct file name in Chrome only...
+          p <- drawVolcano()
+          print(p)
+          dev.off()
+          #ggsave(filename=file, plot=p)
+        }
+      )
+      
     })
     
-   # export Volcano plot
-   output$downloadVolcano <- downloadHandler(
-     filename = function(){paste("volcano", input$download_type_heat, sep = ".")},
-     content = function(file){
-       p <- drawVolcano()
-       export(p, file)
-     }
-   )
    
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+# runApp(launch.browser=TRUE)
 
